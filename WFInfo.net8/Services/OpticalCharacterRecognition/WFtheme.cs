dd2798eb8a53b59fa543141583f2d38861d953be
Rudy Alex Kohn <rudzen@gmail.com>
2024-02-16 21:49:57 +0100

    added extension class to WFTheme

--- WFInfo.net8/Services/OpticalCharacterRecognition/WFtheme.cs
+++ WFInfo.net8/Services/OpticalCharacterRecognition/WFtheme.cs
@@ -1,4 +1,6 @@
-﻿namespace WFInfo.Services.OpticalCharacterRecognition;
+﻿using System.Runtime.CompilerServices;
+
+namespace WFInfo.Services.OpticalCharacterRecognition;
 
 public enum WFtheme
 {
@@ -20,4 +22,36 @@ public enum WFtheme
     UNKNOWN = -1,
     AUTO = -2,
     CUSTOM = -3
+}
+
+public static class WFthemeExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int AsInt(this WFtheme t) => (int)t;
+
+    public static string ToFriendlyString(this WFtheme theme)
+    {
+        return theme switch
+        {
+            WFtheme.VITRUVIAN => "Vitruvian",
+            WFtheme.STALKER => "Stalker",
+            WFtheme.BARUUK => "Baruuk",
+            WFtheme.CORPUS => "Corpus",
+            WFtheme.FORTUNA => "Fortuna",
+            WFtheme.GRINEER => "Grineer",
+            WFtheme.LOTUS => "Lotus",
+            WFtheme.NIDUS => "Nidus",
+            WFtheme.OROKIN => "Orokin",
+            WFtheme.TENNO => "Tenno",
+            WFtheme.HIGH_CONTRAST => "High Contrast",
+            WFtheme.LEGACY => "Legacy",
+            WFtheme.EQUINOX => "Equinox",
+            WFtheme.DARK_LOTUS => "Dark Lotus",
+            WFtheme.ZEPHYR => "Zephyr",
+            WFtheme.UNKNOWN => "Unknown",
+            WFtheme.AUTO => "Auto",
+            WFtheme.CUSTOM => "Custom",
+            _ => throw new ArgumentOutOfRangeException(nameof(theme), theme, null)
+        };
+    }
 }
